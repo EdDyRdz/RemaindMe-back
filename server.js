97ca9b3ef34ee322b189f7acd08c1e26a2638101
Daniel <er610817@gmail.com>
2025-04-11 19:29:38 -0600

    server

--- server.js
+++ server.js
@@ -495,6 +495,6 @@ app.post("/api/requestPasswordChange", authenticate, async (req, res) => {
     }
   });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
