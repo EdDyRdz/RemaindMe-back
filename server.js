59e55d4e07ebf8ca6d15252fe6df3b894c23a1f9
Daniel <er610817@gmail.com>
2025-04-11 19:43:58 -0600

    zi

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://remaindme-front.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); // Para cookies
+  next();
+});
+
 const express = require("express");
 const cors = require("cors");
 const conectarDB = require("./database");
@@ -17,12 +25,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: [
+    "https://remaindme-front.onrender.com", // Dominio de producción
+    "http://localhost:3000" // Para desarrollo local
+  ],
+  credentials: true, // Si usas cookies/tokens
+  methods: ["GET", "POST", "PUT", "DELETE"]
   }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const authenticate = (req, res, next) => {
     const token = req.headers.authorization;
